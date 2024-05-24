fca04cc3786418eed540ea37b7a6e696b5c80f6f
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2024-05-24 14:25:08 +0700

    fix bug filename

---
+++ application/helpers/customize_helper.php
@@ -0,0 +1,9 @@
+<?php
+
+function seatCount()
+{
+    // $CI = &get_instance();
+    // $CI->load->model('seat_model');
+    // $seats = $CI->seat_model->get_all();
+    // return $seats->num_rows();
+}
